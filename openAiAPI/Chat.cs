ccca5d2a4a5c0f5a3b1888970c349defc76ce051
jonatan-m <jonatan.malander@gmail.com>
2024-05-28 14:29:35 +0200

    copy paste

---
+++ openAiAPI/Chat.cs
@@ -0,0 +1,104 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Azure.AI.OpenAI;
+using Azure;
+
+
+namespace openAiAPI
+{
+    public class Chat(ILogger<Chat> logger)
+    {
+        private static string aiUri = Environment.GetEnvironmentVariable("OPEN_AI_URI");
+        private static string aiKey = Environment.GetEnvironmentVariable("OPEN_AI_KEY");
+
+        private static string aiSearchUri = Environment.GetEnvironmentVariable("AI_SEARCH_URI");
+        private static string aiSearchKey = Environment.GetEnvironmentVariable("AI_SEARCH_KEY");
+
+        private static readonly string _deploymentName = Environment.GetEnvironmentVariable("DEPLOYMENT_NAME");
+
+
+        private static OpenAIClient _openAIClient;
+
+        private static AzureSearchChatExtensionConfiguration _searchConfig;
+
+        private readonly ILogger<Chat> _logger = logger;
+
+
+        [Function("chat")]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        {
+            if(_deploymentName is null) { return new StatusCodeResult(500); }
+
+            try
+            {
+                Uri openAiUri = new(aiUri);
+                AzureKeyCredential openAiKey = new(aiKey);
+                Uri searchUri = new(aiSearchUri);
+                OnYourDataApiKeyAuthenticationOptions searchKey = new(aiSearchKey);
+
+                _openAIClient = new(openAiUri, openAiKey);
+                _searchConfig = new()
+                {
+                    SearchEndpoint = searchUri,
+                    Authentication = searchKey,
+                    IndexName = "PLACEHOLDER",
+                    DocumentCount = 43,
+                };
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+                return new StatusCodeResult(500);
+            }
+
+            ChatRequest? chatRequest = await JsonSerializer.DeserializeAsync<ChatRequest>(req.Body);
+
+            if (chatRequest is null)
+            {
+                return new BadRequestResult();
+            }
+
+            var chatOptions = new ChatCompletionsOptions()
+            {
+                DeploymentName = _deploymentName,
+                Messages =
+                {
+                    new ChatRequestSystemMessage("PLACEHOLDER"),
+                    new ChatRequestUserMessage(chatRequest.Message)
+                }
+                //,
+                //AzureExtensionsOptions = new AzureChatExtensionsOptions()
+                //{
+                //    Extensions = { _searchConfig }
+                //}
+            };
+
+
+            Response<ChatCompletions> response = await _openAIClient.GetChatCompletionsAsync(chatOptions);
+
+            try
+            {
+                ChatResponseMessage responseMessage = response.Value.Choices.FirstOrDefault().Message;
+
+                return new OkObjectResult(responseMessage.Content);
+            }
+            catch { return new BadRequestResult(); }
+
+        }
+
+    }
+
+
+    public class ChatRequest
+    {
+        [JsonPropertyName("message")]
+        public string? Message { get; set; }
+
+    }
+
+    
+}
